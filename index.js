27646845f46bd654902fbd9798f1d2f39e951727
jecestanislao10 <jestanislao@stratpoint.com>
2020-03-12 14:55:07 +0800

    refactored

--- index.js
+++ index.js
@@ -1,49 +1,38 @@
 const passport = require('passport');
 const {ExtractJwt, Strategy} = require('passport-jwt');
-let repository, accessKey;
 // const userRepository = require('src/infra/repositories/UserRepository');
 // const userModel = require('src/infra/models/UserModel');
 
-exports.initialize = (key, UserRepository) => {
-  accessKey = key;
-  repository = UserRepository;
+exports.initialize = () => {
   return passport.initialize();
 };
 
 exports.authenticate = () => {
-  return [
-    (req, res, next) => {
+  return passport.authenticate();
+}
 
-      const jwtOptions = {};
-      jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-      jwtOptions.secretOrKey = accessKey;  
-      
+exports.configure = (config, findById) => {
+  const jwtOptions = {};
+  jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+  jwtOptions.secretOrKey = config.authSecret; 
 
-      passport.use(new Strategy(jwtOptions, (jwt_payload, done) => {
-        repository.findByPk(jwt_payload.id)
-          .then((user) => {
-            done(null, user);
-          })
-          .catch((error) =>  done(error, null));
-      }));
+  passport.use(new Strategy(jwtOptions, (jwt_payload, done) => {
+        
+    console.log('payload is', jwt_payload);
+
+    findById(jwt_payload.id).then((user)=> {
+      done(null, user);
+    }).catch((err) => {
+      done(null, err);
+    });
+        
+  }));
+
+  passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
   
-      passport.serializeUser(function (user, done) {
-        done(null, user);
-      });
-  
-      passport.deserializeUser(function (user, done) {
-        done(null, user);
-      });
-  
-      return passport.authenticate('jwt', (err, user, info)=> {
-        if(!user){
-          res.status(401).json({
-            status: 401,
-            message: 'Not Authenticated'
-          });
-        }
-        next();
-      })(req, res, next);
-    }
-  ];
+  passport.deserializeUser(function (user, done) {
+    done(null, user);
+  });
 };
